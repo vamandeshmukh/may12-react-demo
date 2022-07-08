9807d078f9f60f51805284235d2a4a0551085bba
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-08 17:02:54 +0530

    ReactJS

--- src/components/EmpData.js
+++ src/components/EmpData.js
@@ -1,37 +1,136 @@
-
-// state - one object for one component 
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const EmpData = () => {
 
-    // let myName = '';
-    let [myName, setMyName] = useState('');
+    let [emp, setEmp] = useState({});
+
+    useEffect(() => {
+        setEmp({
+            eid: '',
+            firstName: '',
+            salary: ''
+        });
+    }, []);
 
     const handleChange = (evt) => {
-        // myName = evt.target.value; // this does not work 
-        setMyName(evt.target.value); // this works 
-
         console.log(evt.target.value);
-        console.log(evt.target.name);
+        // setEmp(...emp[evt.target.name] evt.target.value);
+    }
+
+    const sumbitData = (evt) => {
+        console.log(evt.target.value);
+        // code 
     }
 
     return (
         <div>
             <h1>EmpData Component</h1>
-            <p>Please enter your name:</p>
+            <p>Please enter Employee data:</p>
             <input
-                type="text"
-                name="myName"
-                value={myName}
+                // className="text-primary"
+                type="number"
+                id="eid"
+                name="eid"
+                value={emp.eid}
+                placeholder="Enter eid"
                 onChange={handleChange} >
             </input>
-            <p>Welcome {myName}! </p>
+            <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={emp.firstName}
+                placeholder="Enter firstname"
+                onChange={handleChange} >
+            </input>
+            <input
+                type="number"
+                id="salary"
+                name="salary"
+                value={emp.salary}
+                placeholder="Enter salary"
+                onChange={handleChange} >
+            </input>
+            <input
+                type="button"
+                value="Submit"
+                onClick={sumbitData}>
+            </input>
         </div>
     );
 }
 
 export default EmpData;
+
+
+// import { useState } from "react";
+
+// const EmpData = () => {
+
+//     let [myName, setMyName] = useState('');
+
+//     // using useState() any type of state variable can be created
+//     let [numberVariable, setNumberVariable] = useState(0);
+//     let [stringVariable, setStringVariable] = useState('');
+//     let [booleanVariable, setBooleanVariable] = useState(false);
+//     let [arrayVariable, setArrayVariable] = useState([]);
+//     let [objectVariable, setObjectVariable] = useState({});
+
+//     const handleChange = (evt) => {
+//         setMyName(evt.target.value); // this works
+//     }
+
+//     return (
+//         <div>
+//             <h1>EmpData Component</h1>
+//             <p>Please enter your name:</p>
+//             <input
+//                 type="text"
+//                 name="myName"
+//                 value={myName}
+//                 onChange={handleChange} >
+//             </input>
+//             <p>Welcome {myName}! </p>
+//         </div>
+//     );
+// }
+
+// export default EmpData;
+
+
+// // state - one object for one component
+
+// import { useState } from "react";
+
+// const EmpData = () => {
+
+//     // let myName = '';
+//     let [myName, setMyName] = useState('');
+
+//     const handleChange = (evt) => {
+//         // myName = evt.target.value; // this does not work
+//         setMyName(evt.target.value); // this works
+
+//         console.log(evt.target.value);
+//         console.log(evt.target.name);
+//     }
+
+//     return (
+//         <div>
+//             <h1>EmpData Component</h1>
+//             <p>Please enter your name:</p>
+//             <input
+//                 type="text"
+//                 name="myName"
+//                 value={myName}
+//                 onChange={handleChange} >
+//             </input>
+//             <p>Welcome {myName}! </p>
+//         </div>
+//     );
+// }
+
+// export default EmpData;
 
 
 // const EmpData = () => {
