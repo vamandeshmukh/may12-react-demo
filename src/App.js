8640c0bae2602a760b36b41bdc6dcfd3b93102eb
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-08 15:22:33 +0530

    ReactJS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 
-const App = () => {
-  return (
-    <div>
-      <h1>CG React App</h1>
-      <p>Welcome to Capgemini ReactJS appliation.</p>
-    </div>
-  );
+// Important React Concepts
+// component, JSX, props, state 
+
+// routing, service, redux, thunk, ... 
+
+// types of components - 2
+// class component // 
+// function component // use this 
+
+// // function component 
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>CG React App</h1>
+//       <p>Welcome to Capgemini ReactJS appliation.</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+// class component 
+import React from "react";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>CG React App</h1>
+        <p>Welcome to Capgemini ReactJS appliation.</p>
+      </div >
+    );
+  }
 }
 export default App;
-
 
 
 // function App() {
