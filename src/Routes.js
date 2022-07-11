a9a0bf3226009e8f045fc9d7f3e2258f09a42312
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-11 12:58:46 +0530

    ReactJS

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,57 @@
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import EmpData from './components/EmpData';
+import Login from './components/Login';
+import Parent from './components/Parent';
 
 const Routes = () => {
 
     return (
         <div className="container-fluid">
             <p>Routes Component</p>
-            {/* base url - http://localhost:3000/
+            <div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path='/login'> <Login />  </Route>
+                        <Route path='/empdata'> <EmpData />  </Route>
+                        <Route path='/parent'> <Parent />  </Route>
+                    </Switch>
+                </BrowserRouter>
 
-            http://localhost:3000/home
-            http://localhost:3000/login
-            http://localhost:3000/empdata
-            http://localhost:3000/parent
 
-            if /login then display login component  
-            if /empdata then display empData component  
-            if /parent then display parent component 
-            */}
+            </div>
+
+
+
         </div >
     );
 }
 
 export default Routes;
+
+
+
+
+
+// const Routes = () => {
+
+//     return (
+//         <div className="container-fluid">
+//             <p>Routes Component</p>
+//             {/* base url - http://localhost:3000/
+
+//             http://localhost:3000/home
+//             http://localhost:3000/login
+//             http://localhost:3000/empdata
+//             http://localhost:3000/parent
+
+//             if /login then display login component
+//             if /empdata then display empData component
+//             if /parent then display parent component
+//             */}
+//         </div >
+//     );
+// }
+
+// export default Routes;
 
 
