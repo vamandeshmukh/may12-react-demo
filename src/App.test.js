aa4f3ba7ef9d3e40a92436af673353b4747d0309
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-14 13:05:20 +0530

    ReactJS

--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,53 @@
+
 import { render, screen } from '@testing-library/react';
-import App from './App';
-import store from './redux/store';
 import { Provider } from 'react-redux';
+import App from './App';
 import EmpData from './components/EmpData';
 import Parent from './components/Parent';
+import store from './redux/store';
 
+// syntax 
+// test(arg1, arg2);
+// test('test description in English', () => { });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders EmpData component properly', () => {
-  render(
-    <Provider store={store}>
-      <EmpData />
-    </Provider>
-  );
-  const abc = screen.getByText('Add New Employee');
-  expect(abc).toBeInTheDocument();
-});
-
-test('renders Parent component properly', () => {
+test('Parent component renders properly.', () => {
   render(
     <Provider store={store}>
       <Parent />
     </Provider>
   );
-  const abc = screen.getByText('This is Parent component.');
-  expect(abc).toBeInTheDocument();
+  const dataToTest = screen.getByText('This is Parent component.');
+  expect(dataToTest).toBeInTheDocument();
 });
+
+test('EmpData component renders properly.', () => {
+  render(
+    <Provider store={store}>
+      <EmpData />
+    </Provider>
+  );
+  const dataToTest = screen.getByText('Add New Employee');
+  expect(dataToTest).toBeInTheDocument();
+});
+
+// test('EmpData component renders properly.', () => {
+//   render(
+//     <Provider store={store}>
+//       <EmpData />
+//     </Provider>
+//   );
+//   const dataToTest = screen.getByText('Add New Customer');
+//   expect(dataToTest).not.toBeInTheDocument();
+// });
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
 
 
 
